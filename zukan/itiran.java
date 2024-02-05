22811159d318d29f3a8cefc9c6c99cafe903a0fd
myuaaryve <ktq2390322@stu.o-hara.ac.jp>
2024-02-05 14:35:38 +0900

    Commit

--- zukan/itiran.java
+++ zukan/itiran.java
@@ -14,28 +14,44 @@ public class Itiran{
                 // 1を入力したらkyubiに移動
                 Memo kyubi = new Memo();
                 kyubi.kyubi();
+                back();
             }
             if(con==2){
                 System.out.println("烏天狗");
                 // 2を入力したらkarasutenguに移動
                 Memo karasutengu = new Memo();
                 karasutengu.karasutengu();
+                back();
             }
             if (con==3) {
                 System.out.println("饕餮");
                 // 3を入力したらtoutetuに移動
                 Memo toutetu = new Memo();
                 toutetu.toutetu();
+                back();
             }
             if (con==4){
                 System.out.println("猫又");
                 // 4を入力したらnekomataに移動
                 Memo nekomata = new Memo();
                 nekomata.nekomata();
+                back();
             }
         }catch(ArithmeticException e){
             // 数字以外が入力されると例外処理
             System.out.println("数字を入力してください!");
+        }
+    }
+    public void back(){
+        System.out.println("1:一覧に戻る,2:メニューに戻る");
+        Scanner num = new Scanner(System.in);
+        int s = num.nextInt();
+        if (s==1) {
+            overview();
+        }
+        if (s==2){
+            Root root = new Root();
+            root.main(null);
         }
     }
 }
--- zukan/rank.java
+++ zukan/rank.java
@@ -6,19 +6,22 @@ public class Rank {
         try{
             Scanner stdIn = new Scanner(System.in);
             System.out.println("見たいランキングを選んでください");
-            System.out.println("\n1:能力の強さ\n2:知性の高さ");
+            System.out.println("\n1:能力の強さ\n2:知性の高さ\n3:危険度");
             Integer con = stdIn.nextInt();
             if (con==1){
                 System.out.println("能力の強さ");
-                
+                // 1を入力したらabilityに移動
+                ability();
             }
             if(con==2){
                 System.out.println("知性の高さ");
                 // 2を入力したらintelligenceに移動
+                intelligence();
             }
-            if(con==4){
-                System.out.println("危険度ランキング");
+            if(con==3){
+                System.out.println("危険度");
                 // 3を入力したらdangerに移動
+                danger();
             }
         }catch(ArithmeticException e){
             System.out.println("数字を入力してください!");
@@ -27,14 +30,20 @@ public class Rank {
 
     public void ability(){
         // 能力順位を表示
-        System.out.println("1位:饕餮 \n2位:烏天狗 \n3位:九尾");
+        System.out.println("1位:饕餮 \n2位:烏天狗 \n3位:九尾\n4位:猫又");
+        Itiran itiran = new Itiran();
+        itiran.back();
     }
     public void intelligence(){
         // 知能順位を表示
-        System.out.println("1位:烏天狗 \n2位:九尾 \n3位:猫又");
+        System.out.println("1位:烏天狗 \n2位:九尾 \n3位:猫又\n4位:饕餮");
+        Itiran itiran = new Itiran();
+        itiran.back();
     }
     public void danger(){
         //　危険度順位を表示
         System.out.println("1位:饕餮 \n2位:九尾 \n3位:烏天狗 \n4位:猫又");
+        Itiran itiran = new Itiran();
+        itiran.back();
     }
 }
